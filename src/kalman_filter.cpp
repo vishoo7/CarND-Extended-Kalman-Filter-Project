fae3d7c46db5dcd8d19d05ac6196905ca17f532f
Vishal <vishal_agrawal@ymail.com>
2017-06-18 14:09:54 -0400

    Implemented Update + Predict

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -19,16 +19,30 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 
 void KalmanFilter::Predict() {
   /**
-  TODO:
-    * predict the state
+  I implemented this from https://classroom.udacity.com/nanodegrees/nd013/parts/40f38239-66b6-46ec-ae68-03afd8a601c8/modules/0949fca6-b379-42af-a919-ee50aa304e6a/lessons/3612b91d-9c33-47ad-8067-a572a6c93837/concepts/a0604e14-2832-4646-835a-05f972453f3d
+  u is 0, 0 because the noise mean is 0
   */
+  VectorXd u(2);
+  u << 0, 0;
+  x_ = F_ * x_ + u;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
-  TODO:
-    * update the state by using Kalman Filter equations
+  I Implemented this from https://classroom.udacity.com/nanodegrees/nd013/parts/40f38239-66b6-46ec-ae68-03afd8a601c8/modules/0949fca6-b379-42af-a919-ee50aa304e6a/lessons/3612b91d-9c33-47ad-8067-a572a6c93837/concepts/a0604e14-2832-4646-835a-05f972453f3d
   */
+  VectorXd y = z - H_ * x_;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P_ * Ht * Si;
+
+  //new state
+  x_ = x_ + (K * y);
+  MatrixXd I = MatrixXd::Identity(2, 2);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
